8acddeb4281dba6e537879adb97d1b83e0d0f951
sneha-rachel-philip <sneharay.mannil@gmail.com>
2025-04-17 23:27:59 +0200

    fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: import.meta.env.VITE_BASE_PATH || "/gig-space",
+  base: process.env.VITE_BASE_PATH || "/gig-space",
   server: {
     proxy: {
       '/api': {
